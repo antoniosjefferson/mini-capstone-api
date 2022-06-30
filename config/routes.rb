5a0fda34b6abcb9909ad2f7a62646162a65a88bd
antoniosjefferson <itsmehtone@gmail.com>
2022-06-30 10:30:07 -0700

    Adds seed information for products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/all_products", controller: "products", action: "all_contacts"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/all_products", controller: "products", action: "all_contacts"
+
+  get "/all_products", controller: "products", action: "all_contacts"
+
+  get "/all_products", controller: "products", action: "all_contacts"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "Stephen Curry Jersey", price: 80, image_url: "https://store.nba.com/golden-state-warriors/mens-golden-state-warriors-stephen-curry-adidas-royal-player-swingman-road-jersey/t-14588485+p-7818287531570+z-9-2210632981", description: "Blue Stephen Curry Jersey #30")
+product.save
+
+product = Product.new(name: "Klay Thompson Jersey ", price: 80, image_url: "https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_3773000/altimages/ff_3773163-4d5ec13b79a730a4fce6alt1_full.jpg&w=900", description: "Yellow Klay Thompson Jersey #11")
+product.save
+
+product = Product.new(name: "Splash Bros Tshirt", price: 40, image_url: "https://ih1.redbubble.net/image.3311329334.7871/ssrco,classic_tee,womens,f89f2b:1617e563f0,front_alt,square_product,600x600.jpg", description: "Splash Bros Tshirt with Stephen Curry and Klay Thompson")
+product.save
+
+product = Product.new(name: "Warriors 2022 Champs Hat", price: 20, image_url: "https://fanatics.frgimages.com/FFImage/thumb.aspx?i=/productimages/_4898000/altimages/ff_4898771-f0e2ba994929555d48caalt1_full.jpg&w=900", description: "Warriors hat with championship logo")
+product.save
